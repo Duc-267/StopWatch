ff7209b4fac00434fa58384779f442461592a8c9
Duc-267 <mduc2672002@gmail.com>
2021-09-13 22:38:25 +0700

    make start stop reset

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,11 +1,72 @@
 package com.example.stopwatch
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.stopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var isRunning = false
+    private lateinit var binding : ActivityMainBinding
+    private var time = 0.0
+    private val intentService = Intent(this, TimeService::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding.btnStartStop.setOnClickListener {
+            timerStartStop()
+        }
+        binding.btnReset.setOnClickListener {
+            resetTimer()
+        }
+        registerReceiver(updateTime(), IntentFilter("updateTime") )
     }
+    private fun timerStartStop(){
+        if(isRunning){
+            stopTimer()
+        } else {
+            startTimer()
+        }
+    }
+    private fun startTimer(){
+        intentService.putExtra("EXTRA_TIME", time)
+        startService(intentService)
+        binding.btnStartStop.text = "Stop"
+        binding.btnStartStop.icon =  getDrawable(R.drawable.ic_baseline_stop_24)
+        isRunning = true
+    }
+
+    private fun stopTimer(){
+        stopService(intentService)
+        isRunning = false
+        time = 0.0
+        binding.btnStartStop.text = "Start"
+        binding.btnStartStop.icon =  getDrawable(R.drawable.ic_baseline_play_arrow_24)
+    }
+
+    private  fun updateTime():BroadcastReceiver = object :BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            time = intent.getDoubleExtra("EXTRA_TIME",0.0)
+            binding.timeView.text = getStingOfTime(time)
+        }
+
+    }
+    private fun resetTimer(){
+        stopTimer()
+        binding.timeView.text = getStingOfTime(0.0)
+    }
+
+    private fun getStingOfTime(time: Double): String{
+        val timeToInt = time.toInt()
+        val second = timeToInt % 60
+        val minute = (timeToInt - second) / 60
+        val hour = (timeToInt - second) / 3600
+        return makeTimeString(hour, minute, second)
+    }
+    private fun makeTimeString(hour: Int, min: Int, sec: Int): String =
+        String.format("%02d:%02d:%02d", hour, min, sec)
 }
---
+++ app/src/main/java/com/example/stopwatch/TimeService.kt
@@ -0,0 +1,36 @@
+package com.example.stopwatch
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.widget.Toast
+import java.util.*
+
+class TimeService: Service() {
+    private val timer = Timer()
+    init {
+        Toast.makeText(this, "Start Stopwatch", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onBind(p0: Intent?): IBinder? = null
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val time = intent.getDoubleExtra("EXTRA_TIME", 0.0)
+        timer.scheduleAtFixedRate(TimeTask(time), 0, 1000)
+        return START_NOT_STICKY
+    }
+
+    private inner class TimeTask(private var time: Double):TimerTask(){
+        override fun run() {
+            val intent = Intent("timeUpdate")
+            time++
+            intent.putExtra("EXTRA_TIME", time)
+            sendBroadcast(intent)
+        }
+
+    }
+    override fun onDestroy() {
+        timer.cancel()
+        super.onDestroy()
+    }
+}
